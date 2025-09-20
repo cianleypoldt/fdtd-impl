09660b386473b780bebbb05a500d06c76cfd740c
Cian Leypoldt <cianleypoldt@gmail.com>
2025-09-20 20:02:39 +0200

    comments

--- main.c
+++ main.c
@@ -9,23 +9,22 @@ double *field_mem;
 
 double *E_x, *E_y, *E_z;
 double *B_x, *B_y, *B_z;
-double *inv_eps;  // precomputed inverse permittivity (1/ε)
-double *inv_mu;   // precomputed inverse permeability (1/μ)
+double *inv_eps;  // (1/ε)
+double *inv_mu;   // (1/μ)
 
 double   dt = -1;
 double   dx = -1, dy = -1, dz = -1;
 unsigned grid_dim_x = 0, grid_dim_y = 0, grid_dim_z = 0;
-double   time = 0;  // current simulation time
+double   time = 0;
 
 void create_field(unsigned dim_x, unsigned dim_y, unsigned dim_z) {
-    time       = 0.0;  // Initialize simulation time
+    time       = 0.0;
     grid_dim_x = dim_x + 1;
     grid_dim_y = dim_y + 1;
     grid_dim_z = dim_z + 1;
 
     unsigned total_elements = grid_dim_x * grid_dim_y * grid_dim_z;
 
-    // single contiguous memory block to be partitioned for field arrays
     size_t total_size = total_elements * 8 * sizeof(double);
     field_mem         = (double *) malloc(total_size);
     if (!field_mem) {
@@ -34,7 +33,6 @@ void create_field(unsigned dim_x, unsigned dim_y, unsigned dim_z) {
     }
     memset(field_mem, 0, total_size);
 
-    // block partitioning
     E_x     = field_mem;
     E_y     = E_x + total_elements;
     E_z     = E_y + total_elements;
@@ -57,7 +55,7 @@ void update_E_field() {
     const unsigned stride_y = grid_dim_z;
     const unsigned stride_z = 1;
 
-    // first and last E field layers updated later as PEC boundary, and idx starts at E_*[1, 1, 1]
+    // first and last E field layers updated later on as PEC boundary, index initialized at E_*[1, 1, 1]
     uint idx = stride_z + stride_x + stride_y;
     for (int i = 1; i < grid_dim_x - 1; i++) {
         for (int j = 1; j < grid_dim_y - 1; j++) {
@@ -77,9 +75,9 @@ void update_B_field() {
     const unsigned stride_x = grid_dim_y * grid_dim_z;
     const unsigned stride_y = grid_dim_z;
     const unsigned stride_z = 1;
+    uint           idx      = 0;
 
-    uint idx = 0;
-    // rear B-field layers are buffers (-> for grid_dim_* - 1)
+    // rear B field layers are buffers (-> for grid_dim_* - 1)
     for (int i = 0; i < grid_dim_x - 1; i++) {
         for (int j = 0; j < grid_dim_y - 1; j++) {
             for (int k = 0; k < grid_dim_z - 1; k++) {
@@ -88,13 +86,13 @@ void update_B_field() {
                 B_z[idx] -= dt * ((E_y[idx + stride_x] - E_y[idx]) / dx - (E_x[idx + stride_y] - E_x[idx]) / dy) * inv_mu[idx];
                 idx++;
             }
-            idx++;        // skip B-field-buffer
+            idx++;        // skip B field-buffer
         }
-        idx += stride_y;  // skip B-field-buffer
+        idx += stride_y;  // skip B field-buffer
     }
 }
 
-// Initialize B field to t = -dt/2 for proper leapfrog starting point
+// Initialize B field to t = -dt/2 for leapfrog starting point
 void initialize_leapfrog() {
     const double   half_dt  = dt * 0.5;
     const unsigned stride_x = grid_dim_y * grid_dim_z;
@@ -110,9 +108,9 @@ void initialize_leapfrog() {
                 B_z[idx] -= half_dt * ((E_y[idx + stride_x] - E_y[idx]) / dx - (E_x[idx + stride_y] - E_x[idx]) / dy) * inv_mu[idx];
                 idx++;
             }
-            idx++;        // skip B-field-buffer
+            idx++;        // skip B field-buffer
         }
-        idx += stride_y;  // skip B-field-buffer
+        idx += stride_y;  // skip B field-buffer
     }
 }
 
